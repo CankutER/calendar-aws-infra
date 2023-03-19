4cb72cd3b1c3629e93a323f3ae8685e7f3ae8e2e
CankutER <cnkt.er@gmail.com>
2023-03-19 23:49:43 +0300

    final

--- lib/calendar-frontend-infra-stack.ts
+++ lib/calendar-frontend-infra-stack.ts
@@ -1,16 +1,39 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { SymlinkFollowMode } from "aws-cdk-lib";
+import { SubnetType, Vpc } from "aws-cdk-lib/aws-ec2";
+import { ContainerImage } from "aws-cdk-lib/aws-ecs";
+import { ApplicationLoadBalancedFargateService } from "aws-cdk-lib/aws-ecs-patterns";
+import { Construct } from "constructs";
+import path = require("path");
 
-export class CalendarFrontendİnfraStack extends cdk.Stack {
+export class CalendarFrontendInfraStack extends cdk.Stack {
+  private frontEndFargate: ApplicationLoadBalancedFargateService;
+  private vpc: Vpc;
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CalendarFrontendİnfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    this.vpc = new Vpc(this, "FrontEndVpc", {
+      maxAzs: 2,
+      subnetConfiguration: [
+        { name: "frontEndPublicSubnet", subnetType: SubnetType.PUBLIC },
+      ],
+    });
+    this.frontEndFargate = new ApplicationLoadBalancedFargateService(
+      this,
+      "FrontEndFargate",
+      {
+        assignPublicIp: true,
+        vpc: this.vpc,
+        taskSubnets: { subnetType: SubnetType.PUBLIC },
+        desiredCount: 1,
+        publicLoadBalancer: true,
+        taskImageOptions: {
+          containerName: "FrontEndContainer",
+          containerPort: 80,
+          image: ContainerImage.fromAsset(
+            path.resolve(__dirname, "..", "..", "Calendar-Frontend")
+          ),
+        },
+      }
+    );
   }
 }
